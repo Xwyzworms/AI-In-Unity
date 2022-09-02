850ce17c8a9b2e1d24ad3a1154818332a745cd01
Xwyzworms <pratamaazmi1@gmail.com>
2022-09-03 06:48:21 +0700

    add Vector Basic implementation

---
+++ MathematicsBasic/Vectors/Assets/Move.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public GameObject goal;
+    public Vector3 direction;
+    public float speeds;
+
+    public void Start() 
+    {
+    }
+
+
+    public void LateUpdate() 
+    {
+
+        direction = goal.transform.position - this.transform.position;
+        if(direction.magnitude > 2) 
+        {   
+            this.transform.LookAt(goal.transform);
+            Vector3 velocity = direction.normalized * speeds * Time.deltaTime;
+            this.transform.position = this.transform.position + velocity;
+
+        }
+    }
+
+}
+
