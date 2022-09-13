bf15415a67b25c55733e6a37b5acf57455ae19c5
Xwyzworms <pratamaazmi1@gmail.com>
2022-09-13 20:40:14 +0700

    Waypoints, changed using Tracker, so the obj follow tracker instead env

--- Waypoints/Waypoints/Assets/FollowWaypoint.cs
+++ Waypoints/Waypoints/Assets/FollowWaypoint.cs
@@ -4,21 +4,52 @@ using UnityEngine;
 
 public class FollowWaypoint : MonoBehaviour
 {
-    public float speed = 20.0f;
+    public float speed = 10.0f;
+    public float rotationSpeed = 10.0f;
+    public float tracker_additionSpeed = 20;
     public GameObject[] waypoints;
+    
+    private GameObject tracker;
     private int wpLoc=0;
     // Update is called once per frame
+    //
+    //
+    
+
+    void ProgressTracker()
+    {
+        if(Vector3.Distance(tracker.transform.position, this.transform.position) >= 10) return;
+
+        if(Vector3.Distance(tracker.transform.position, waypoints[wpLoc].transform.position) < 3) 
+        {
+            wpLoc +=1 ;
+            
+            if(wpLoc >= waypoints.Length) 
+            {
+                wpLoc = 0;
+            }
+        }
+        tracker.transform.LookAt(waypoints[wpLoc].transform);
+        tracker.transform.Translate(0, 0, (speed + tracker_additionSpeed) * Time.deltaTime);
+
+    }
+    void Start() 
+    {
+        tracker = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        DestroyImmediate(tracker.GetComponent<Collider>());
+        tracker.transform.position = this.transform.position;
+        tracker.GetComponent<MeshRenderer>().enabled =false;
+        tracker.transform.rotation = this.transform.rotation;
+            
+    }
     void Update()
     {
         if(waypoints.Length != 0) {
-            float distance = Vector3.Distance(this.transform.position, waypoints[wpLoc].transform.position);
-            if(distance < 3) 
-            {
-                wpLoc ++;
-            }
-            if(wpLoc >= waypoints.Length) wpLoc=0;
+            ProgressTracker();
 
-            this.transform.LookAt(waypoints[wpLoc].transform);
+            Quaternion wpLookat = Quaternion.LookRotation(tracker.transform.position - this.transform.position);
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, wpLookat, rotationSpeed * Time.deltaTime);
+//          this.transform.LookAt(waypoints[wpLoc].transform);
             this.transform.Translate(0, 0, speed * Time.deltaTime);
         }
     }
